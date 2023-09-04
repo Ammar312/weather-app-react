1e8d8a890225a26c4346a348b67804f67e4c05c8
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2023-09-04 17:26:32 +0500

    use of useEffect

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,21 +4,21 @@ const Card = ({ weatherData }) => {
   return (
     <div>
       <div className=" flex flex-col border-2 border-red-200 rounded-md">
-        <div id="cityname">{weatherData.name}</div>
+        <div id="cityname">{weatherData?.name}</div>
         <span id="dateandtime"></span>
         <div id="imgandtemp">
           <div id="img"></div>
           <div id="temp">
             <div>
-              <h1>{weatherData.main.temp}</h1>
+              <h1>{weatherData?.main?.temp}</h1>
             </div>
-            <span id="currentcondition">{weatherData.weather[0].main}</span>
+            {/* <span id="currentcondition">{weatherData?.weather[0]?.main}</span> */}
           </div>
         </div>
         <div id="">
-          <div id="wind">{weatherData.wind.speed}</div>
-          <div id="humidity">{weatherData.main.humidity}</div>
-          <div id="visibility">{weatherData.visibility}</div>
+          <div id="wind">{weatherData?.wind?.speed}</div>
+          <div id="humidity">{weatherData?.main?.humidity}</div>
+          <div id="visibility">{weatherData?.visibility}</div>
         </div>
       </div>
     </div>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,36 @@
 import axios from "axios";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Card from "./Card";
 
 const Search = () => {
   const cityNameRef = useRef(null);
-  const [weatherData, setWeatherData] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+  // const [currentLocationWeather, setCurrentLocationWeather] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    setIsLoading(true);
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(async (location) => {
+        console.log("location: ", location);
+
+        const myApi = "7a08a0e9f8541685c4151b60c35c2b50";
+        try {
+          const response = await axios.get(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${location.coords.latitude}&lon=${location.coords.longitude}&appid=${myApi}&units=metric`
+          );
+          console.log(response.data);
+
+          setWeatherData(response.data);
+          setIsLoading(false);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   const submitHandler = async (e) => {
     e.preventDefault();
     const myApi = "7a08a0e9f8541685c4151b60c35c2b50";
@@ -20,7 +46,9 @@ const Search = () => {
       console.log("hours", hours);
       //   console.log(response.data.dt);
       setWeatherData(response.data);
+      setIsLoading(false);
     } catch (error) {
+      setIsLoading(false);
       console.log(error);
     }
   };
@@ -41,7 +69,12 @@ const Search = () => {
           <i className="bi bi-search"></i>
         </button>
       </form>
-      {weatherData ? <Card weatherData={weatherData} /> : <div>No Data</div>}
+      {isLoading ? <div>Loading...</div> : null}
+      {weatherData || isLoading ? null : <div>No Data</div>}
+      <Card weatherData={weatherData} />
+      {/* {currentLocationWeather ? (
+        <Card weatherData={currentLocationWeather} />
+      ) : null} */}
     </div>
   );
 };
